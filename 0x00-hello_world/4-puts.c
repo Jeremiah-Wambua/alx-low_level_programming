92daf04abd761e3fb03b7d45a9ed16a5a944f574
Jeremiah_Wambua <jeremutinda020@gmail.com>
2022-09-11 23:12:55 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,10 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *   Description: prints "Programming is like building a multilingual puzzle"
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- * */
+ * main -Entry point
+ * Description: print "Programming is like building a muiltilingual puzzle"
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
